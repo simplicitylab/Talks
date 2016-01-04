2c3d8461db3f2dc7590f34f025ea82528567ccde
Glenn De Backer <glenn@simplicity.be>
2016-01-04 18:21:22 +0100

    Added stopwords example

--- text-classification-php/stopwords.php
+++ text-classification-php/stopwords.php
@@ -1,20 +1,30 @@
 <?php
 /**
- * Example of tokenizing using NlpTools Tokenizer
+ * Example of using stopwords when using NlpTools
  *
  * @author Glenn De Backer <glenn@simplicity.be>
  */
 include('vendor/autoload.php');
 
 use \NlpTools\Tokenizers\WhitespaceAndPunctuationTokenizer;
+use \NlpTools\Documents\TokensDocument;
+use \NlpTools\Utils\StopWords;
 
 // text we will be converting into tokens
 $text = "PHP is a server side scripting language designed for web development
 but also used as a general-purpose programming language.";
 
+// define a list of stop words
+$stop = new StopWords(array("is", "a", "as", "for", "also", ".", ",", "!","-"));
+
 // initialize Whitespace and punctuation tokenizer
 $tokenizer = new WhitespaceAndPunctuationTokenizer();
 
-// dump array of tokens
-var_dump($tokenizer->tokenize($text));
+// init token document
+$doc = new TokensDocument($tokenizer->tokenize($text));
+
+// apply our stopwords
+$doc->applyTransformation($stop);
+
+var_dump($doc->getDocumentData());
 ?>
